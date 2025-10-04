d0b1a734f07a25a81473e618740fe865dbd6b326
Thilak-0329 <petrolhead0329@gmail.com>
2025-10-04 02:10:45 -0700

    Update Property.java

--- Property.java
+++ Property.java
@@ -2,39 +2,32 @@ package first;
 import java.util.ArrayList;
 
 public class Property {
-    private int propertyId;
-    private String name;
-    private String address;
-    private ArrayList<Unit> units;
-
-    public Property(int propertyId, String name, String address) {
-        this.propertyId = propertyId;
-        this.name = name;
-        this.address = address;
-        this.units = new ArrayList<>();
-    }
-
-    public void addUnit(Unit unit) {
-        units.add(unit);
-    }
-
-    public void removeUnit(int unitId) {
-        units.removeIf(u -> u.getUnitId() == unitId);
-    }
-
-    public ArrayList<Unit> getUnits() {
-        return units;
-    }
-
-    public ArrayList<Unit> getVacantUnits() {
-        ArrayList<Unit> vacant = new ArrayList<>();
-        for (Unit u : units) {
-            if (u.isVacant()) {
-                vacant.add(u);
-            }
-        }
-        return vacant;
-    }
-
-    // Getters and Setters omitted for brevity
+private int propertyId;
+private String name;
+private String address;
+private ArrayList<Unit> units;
+public Property(int propertyId, String name, String address) {
+this.propertyId = propertyId;
+this.name = name;
+this.address = address;
+this.units = new ArrayList<>();
+}
+public void addUnit(Unit unit) {
+units.add(unit);
+}
+public void removeUnit(int unitId) {
+units.removeIf(u -> u.getUnitId() == unitId);
+}
+public ArrayList<Unit> getUnits() {
+return units;
+}
+public ArrayList<Unit> getVacantUnits() {
+ArrayList<Unit> vacant = new ArrayList<>();
+for (Unit u : units) {
+if (u.isVacant()) {
+vacant.add(u);
+}
+}
+return vacant;
+}
 }
